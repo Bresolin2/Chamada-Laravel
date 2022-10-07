8834b09372b37d2aa4e9d1f42d5a89b77ab37f75
Lucas Bresolin <99617244+Bresolin2@users.noreply.github.com>
2022-10-07 11:06:57 -0300

    inicio

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\AlunoController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/index', [AlunoController::class, 'index'])->name('index');
 
 Route::get('/', function () {
     return view('welcome');
