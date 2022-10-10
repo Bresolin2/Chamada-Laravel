2fd7ef03795b9f92bcadb5b6e730a7a84fa20775
Lucas Bresolin <99617244+Bresolin2@users.noreply.github.com>
2022-10-10 09:33:18 -0300

    att01 10/10

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 //Route::middleware(['auth'])->group(function () {
     Route::get('/index', [AlunoController::class, 'index'])->name('index');
-    Route::get('/index/{id}', [AlunoController::class, 'show'])->name('show');
+    Route::get('/show/{id}', [AlunoController::class, 'show'])->name('show');
 //});
 
 Route::get('/', function () {
