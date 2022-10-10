b8caefd92412593fc5f6f4cefb33e8c6966a24c4
Lucas Bresolin <99617244+Bresolin2@users.noreply.github.com>
2022-10-10 13:25:44 -0300

    att03
    algumas atualizações

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,8 +1,12 @@
-<h1>Detalhes do usuário</h1>
+@extends('layouts.app')
 
-<ul>
+@section('content')
+    <h1>Detalhes do usuário</h1>
+
+    <ul>
         <li>{{ $alunos->nome }}</li><br>
         <li>{{ $alunos->email }}</li><br>
         <li>{{ $alunos->telefone }}</li><br>
         <li>{{ $alunos->foto }}</li><br><br>
-</ul>
+    </ul>
+@endsection
